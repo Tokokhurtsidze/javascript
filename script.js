43e20c46f90840cd95d6cb35dde6c1c7bcad3902
toko khurtsidze <khurtsidzetoko@gmail.com>
2024-11-19 22:58:52 +0400

    javascript2

--- script.js
+++ script.js
@@ -379,34 +379,254 @@
 
 // showQuestion();
 
-const btn = document.querySelector('.btn');
-const modal = document.querySelector('#modal');
-const ads = document.querySelector('#ads');
-const lay = document.querySelector('#overlay');
+// const btn = document.querySelector('.btn');
+// const modal = document.querySelector('#modal');
+// const ads = document.querySelector('#ads');
+// const lay = document.querySelector('#overlay');
 
-btn.addEventListener("click", () => {
-    modal.style.display = "flex";
-    lay.style.display = "flex";
-});
+// btn.addEventListener("click", () => {
+//     modal.style.display = "flex";
+//     lay.style.display = "flex";
+// });
 
-const clossmodall = (element, first, second) => {
-    element.addEventListener("click", () => {
-        first.style.display = "none";
-        second.style.display = "none";
+// const clossmodall = (element, first, second) => {
+//     element.addEventListener("click", () => {
+//         first.style.display = "none";
+//         second.style.display = "none";
+//     });
+// };
+
+// clossmodall(ads, modal, lay);
+// clossmodall(lay, modal, lay);
+
+
+// const input = document.getElementById('numberInput');
+// const button = document.getElementById('calculateBtn');
+// const resultDiv = document.getElementById('result');
+
+// function calculateAverage(input) {
+//     const numbers = input.split(':').map(Number); 
+//     if (numbers.some(isNaN)) return null; 
+//     return numbers.reduce((a, b) => a + b, 0) / numbers.length || 0;
+// }
+
+
+// function showAverage() {
+//     const input = document.getElementById('nums').value; 
+//     const average = calculateAverage(input);
+//     document.getElementById('output').textContent = 
+//         average !== null ? `Average: ${average}` : "Please enter valid numbers!";
+// }
+
+
+// const user = {
+//     name: 'toko',
+//     surname: 'kh',
+//     age:20,
+//     address:function (){
+//        console.log('varancovi');
+       
+//     }
+// }
+// console.log(
+//     user.name,
+//     user.surname,
+//     user.age,
+    
+    
+// );
+// user.address()
+
+// const user10 =user;
+// user10.name='temo'
+
+// console.log(user10.name);
+
+
+// const New=[0,1,2,{name:'sam'},4]
+// for( let i=0;i<New.length;i++){
+//     console.log(New[i]);
+    
+// }
+// console.log(New);
+
+
+// const Saxeli = ['gio','dato','saba','nika']
+
+// for (const names of Saxeli){
+//     console.log(names);
+    
+// }
+
+
+// function round(){
+//     return parseInt(Math.random()*5)+1
+// }
+// let result = round()
+
+// console.log(result);
+ 
+// while(result !==3){
+//     result=round()
+// console.log(result);
+
+// }
+// //  for each
+// const numbers1 = ['geo','eng','usa','ind','frc']
+// numbers1.forEach(function(element,index){
+//     console.log(index + 'text'+element);
+    
+// })
+// // Map
+// const map1 = ['geo','eng','usa','ind','frc']
+// map1.map(function(element,index){
+//     return index + 'text'+element
+    
+// })
+// console.log(map1);
+
+// // Filter
+// const arrays =[12,23,45,67]
+
+// const filter1 =arrays.filter(function(element){
+//     return element >= 35
+// })
+// console.log(arrays);
+
+// console.log(filter1);
+// // Find იმ რიცხვს და იმ ცვლადს ამოიღებს რომელიც არის მასივში ,თუ შეიტან ისეთ რიცხვს ან ცვლადს რომელიც არ არის მასივში გამოიტანს undefinds
+// const find1 = arrays.find(function(el){
+//     return el == 3
+//     // return el >20 გამოიტანს იმ პირველ რიცხვს რომელიც მეტია 20-ზე
+// })
+// console.log(find1);
+
+
+// let numbers = [1, 2, 3];
+// numbers.forEach((num) => console.log(num * 2));
+
+
+// let doubled = numbers.map((num) => num * 2);
+// console.log(doubled);
+
+
+// // ES6 way
+// // object
+// const guest = {
+//     name:'george',
+//     surname:'washington',
+//     from:'Usa'
+// }
+// const {name,surname,from} =guest
+// console.log(name,surname,from);
+// // arrays
+// const randoms = ['usa','france','italy']
+// const [usa,france,italy] = randoms
+// console.log(usa,france,italy);
+// // arrays გამოტოვება
+// const Country= ['Usa','france','Italy']
+// const [a, ,b] = Country
+// console.log(a,b);
+
+
+// const names2 = {
+//     name3:'a',
+//     surname3:'b',
+//     age:22,
+//     address: {
+//       country:'Usa',
+//       city:'DC'
+//     },
+//     preaddress:['georgia','france','italy']
+// }
+// const {name3,surname3,age,address:{country,city},preaddress=[a,,b]} =names2
+// console.log(name,surname,age,country,city,a,b);
+
+// const ff = ({address:{city}}) =>{
+// return "city is" + ' ' + city
+// }
+// console.log(ff(names2));
+
+// // ES6 function
+// // use this one ``
+// const us = (name) => {
+//     return `Hello  ${name}`
+// }
+// console.log(us('Tornike'));
+
+// // better version than last one
+// const uss = name => `Hi ${name}`
+// console.log(uss('Tom'));
+// // თუ მონაცემს ამოვშლით ან არ შევიყვანთ ობიექტსი კოდი არ გაიქრაშება აჩვენებს მხოლოდ undefiend
+// const ob = {
+//     name:'t',
+//     age:40
+// }
+// const {address:{cities}={}}=ob
+// console.log(cities);
+
+
+// // Deep copy
+// const user0 = {
+//     name:'George',
+//     surname:'washington',
+//     from:{
+//         country:'USA',
+//         city:'New-York'
+//     }
+// }
+// const user9={
+//     ...user0,
+//     from:{
+//         ...user0.from
+//     }
+// }
+// user9.from.country='ENG'
+// console.log(user0.from.country);
+// console.log(user9.from.country);
+
+
+// const calculatesum = (...args)=>{
+//     let total=0
+//     for(const num of args){
+//         total+=num;
+//     }
+//     return total
+// }
+// console.log(calculatesum(2,2,3,4,5,6,7,));
+
+
+function mySetTimeout(delay) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, delay);
     });
-};
-
-clossmodall(ads, modal, lay);
-clossmodall(lay, modal, lay);
-
-
-const input = document.getElementById('numberInput');
-const button = document.getElementById('calculateBtn');
-const resultDiv = document.getElementById('result');
-
-function calcAvg() {
-    const nums = document.getElementById('nums').value.split(':').map(Number);
-    const avg = nums.reduce((a, b) => a + b, 0) / nums.length || 0;
-    document.getElementById('output').textContent = `Average: ${avg}`;
+}
+async function makeToy(timeToMake) {
+    console.log("Toy production started...");
+    await mySetTimeout(timeToMake);
+    console.log("Toy production completed!");
 }
 
+async function deliverToys(timeToDeliver) {
+    console.log("Toy delivery started...");
+    await mySetTimeout(timeToDeliver);
+    console.log("Toy delivery completed!");
+}
+
+async function toyShopWorkflow(timeToMake, timeToDeliver) {
+    console.log("Welcome to the Toy Shop!");
+
+    
+    await makeToy(timeToMake);
+
+    
+    await deliverToys(timeToDeliver);
+
+    console.log("All tasks are done!");
+}
+
+
+const timeToMake = 3000; 
+const timeToDeliver = 2000; 
+
+toyShopWorkflow(timeToMake, timeToDeliver);
